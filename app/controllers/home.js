a890b0c7eee395870145d9d49bfec81c0327f4ed
Daniel-PC\Daniel <danielhuamani15@gmail.com>
2015-05-10 18:06:05 -0500

    template y static
    implementacionde template y html

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -3,9 +3,11 @@ var homeController = function(server){
 	//ruuta q respondera el server
 	server.route('/')
 		//se pasa dos parametros peticiones y respuesta
-		.get(function(req, res){
+	.get(function(req, res){
 			//respuesta alcliente
-				res.send("hola mundo")
+				// res.send("hola mundo")
+			//render respuesta al html
+			res.render('home/index');
 		});
 };
 
--- server.js
+++ server.js
@@ -1,8 +1,16 @@
-//cargamos la libreria express
-var express = require('express');
+//cargamos la libreria express, swig
+var express = require('express'),
+		swig = require('swig');
 //instanciamos a express
 var server = express();
 
+//express usara como swig como motor de template y renderiza ; luego que el moto de vista serae html
+server.engine('html', swig.renderFile);
+server.set('view engine', 'html');
+server.set('views', __dirname+ '/app/views');
+
+//usamos el metodo static d e expres le pasamos la ruta
+server.use(express.static('./public'));
 //exportamos home.js
 require('./app/controllers/home')(server);
 
