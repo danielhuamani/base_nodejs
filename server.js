eb433a3e93207df28c9355c047fa3135b89f9732
Daniel-PC\Daniel <danielhuamani15@gmail.com>
2015-05-12 08:52:39 -0500

    passport
    preprar para logueo de redes

--- server.js
+++ server.js
@@ -1,8 +1,31 @@
 //cargamos la libreria express, swig
 var express = require('express'),
-		swig = require('swig');
+		swig = require('swig'),
+		passport = require('passport'),
+		session = require('express-session');
+		cookierParser = require('cookie-parser');
 //instanciamos a express
 var server = express();
+
+//cookier antes de la session
+server.use(cookierParser)
+//se configura session antes de passport
+server.use(session({secret: 'mi clave'}));
+//configuracion de passport
+server.use(passport.initialize());
+server.use(passport.session());
+
+passport.serializeUser()
+
+passport.serializeUser(function(user, done) {
+  done(null, user._id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
 
 //express usara como swig como motor de template y renderiza ; luego que el moto de vista serae html
 server.engine('html', swig.renderFile);
