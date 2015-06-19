c55e4181b2b4aa363a46fe5de71151fa166179d0
Daniel-PC\Daniel <danielhuamani15@gmail.com>
2015-06-19 00:33:34 -0500

    agregados

--- app/model/pregunta.js
+++ app/model/pregunta.js
@@ -1,1 +1,12 @@
-var mongoose = require('../con')
+var mongoose = require('../connections/mongoose')
+var Schema = mongoose.Schema
+
+var preguntaSchema = new Schema({
+	user : {type : Schema.Types.ObjectId, ref : 'user'},
+	titulo : {type : String},
+	content: {type : String}
+
+})
+
+var pregunta = mongoose.model('pregunta', preguntaSchema);
+module.exports = pregunta;
